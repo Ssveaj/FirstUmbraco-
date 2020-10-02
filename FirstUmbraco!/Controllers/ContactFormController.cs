6997b13ba43173f78afd2aebe07a173a2cd1dcb4
Your Name <Your Email>
2020-10-02 11:32:30 +0200

    Fixing ContactFormController

--- FirstUmbraco!/Controllers/ContactFormController.cs
+++ FirstUmbraco!/Controllers/ContactFormController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 using Umbraco.Web.Mvc;
@@ -10,13 +11,37 @@ namespace FirstUmbraco_.Controllers
 {
     public class ContactFormController : SurfaceController
     {
+        public ActionResult RenderContactForm()
+        {
+            return PartialView("_ContactForm", new ContactFormViewModel());
+        }
         [HttpPost]
-        public ActionResult Submit(ContactFormViewModel model)
+        public ActionResult HandleContactForm(ContactFormViewModel model)
         {
             if (!ModelState.IsValid)
-                return CurrentUmbracoPage();
+            {
 
-                
+                return CurrentUmbracoPage();
+            }
+
+            MailMessage email = new MailMessage("noreply@example.local", model.Email)
+            {
+                Subject = "Contact Form Request",
+                Body = model.Message
+            };
+            try
+            {
+                SmtpClient smtp = new SmtpClient();
+
+                smtp.Send(email);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            TempData["IsSuccessful"] = true;
+
             return RedirectToCurrentUmbracoPage();
         }
     }
